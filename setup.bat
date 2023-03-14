932477d8ad93f887d86ac59324885a8fe5de99b8
jiri132 <jiri28beurskens@gmail.com>
2023-03-14 15:09:46 +0100

    Initial commit

---
+++ setup.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: When you dont have the modules downloaded
+IF NOT EXIST node_modules (
+    echo "Installing Application"
+    node
+    pip install typescript
+)
+
+:: When you get past installing Start the application
+echo "Starting Application"
+
+:: First time having typescript used create the config file and main file
+echo "Setting up Typescript"
+IF NOT EXIST tsconfig.json (
+    npx tsc --init
+    tsc src/js/main.ts 
+)
+
+:: Starting the file watchers for typescript
+echo "Starting typescript watcher"
+npx tsc -w
